9ad9de7e7f4ed8bddee3af804efafb0a571f67ca
Boy Hagemann <bh@web1on1.nl>
2017-10-06 23:17:12 +0200

    removed homepage from package.json

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var path    = require("path");
-var app     = require("express");
-
-app.use(express.static(__dirname + '/build/static'));
 
 exports.http = (request, response) => {
   response.status(200).sendFile(path.join(__dirname + '/build/index.html'));
